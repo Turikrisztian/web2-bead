fa36b888f8a5c0b36ef2a9c5a2eba70922699ca5
Turikrisztian <148880706+Turikrisztian@users.noreply.github.com>
2025-11-20 18:47:04 +0100

    Revert "4"
    This reverts commit 3e9557e06c70671c49fbe178cce5c7b93116dfca.

--- InflectorFactory.php
+++ InflectorFactory.php
@@ -2,59 +2,20 @@
 
 declare(strict_types=1);
 
-namespace Doctrine\Inflector;
+namespace Doctrine\Inflector\Rules\French;
 
-use Doctrine\Inflector\Rules\English;
-use Doctrine\Inflector\Rules\Esperanto;
-use Doctrine\Inflector\Rules\French;
-use Doctrine\Inflector\Rules\Italian;
-use Doctrine\Inflector\Rules\NorwegianBokmal;
-use Doctrine\Inflector\Rules\Portuguese;
-use Doctrine\Inflector\Rules\Spanish;
-use Doctrine\Inflector\Rules\Turkish;
-use InvalidArgumentException;
+use Doctrine\Inflector\GenericLanguageInflectorFactory;
+use Doctrine\Inflector\Rules\Ruleset;
 
-use function sprintf;
-
-final class InflectorFactory
+final class InflectorFactory extends GenericLanguageInflectorFactory
 {
-    public static function create(): LanguageInflectorFactory
+    protected function getSingularRuleset(): Ruleset
     {
-        return self::createForLanguage(Language::ENGLISH);
+        return Rules::getSingularRuleset();
     }
 
-    public static function createForLanguage(string $language): LanguageInflectorFactory
+    protected function getPluralRuleset(): Ruleset
     {
-        switch ($language) {
-            case Language::ENGLISH:
-                return new English\InflectorFactory();
-
-            case Language::ESPERANTO:
-                return new Esperanto\InflectorFactory();
-
-            case Language::FRENCH:
-                return new French\InflectorFactory();
-
-            case Language::ITALIAN:
-                return new Italian\InflectorFactory();
-
-            case Language::NORWEGIAN_BOKMAL:
-                return new NorwegianBokmal\InflectorFactory();
-
-            case Language::PORTUGUESE:
-                return new Portuguese\InflectorFactory();
-
-            case Language::SPANISH:
-                return new Spanish\InflectorFactory();
-
-            case Language::TURKISH:
-                return new Turkish\InflectorFactory();
-
-            default:
-                throw new InvalidArgumentException(sprintf(
-                    'Language "%s" is not supported.',
-                    $language
-                ));
-        }
+        return Rules::getPluralRuleset();
     }
 }
--- Version.php
+++ Version.php
@@ -1,69 +1,208 @@
-<?php
-
-declare(strict_types=1);
-
-namespace Faker\Core;
-
-use Faker\Extension;
-use Faker\Provider\DateTime;
-
-/**
- * @experimental This class is experimental and does not fall under our BC promise
+<?php declare(strict_types = 1);
+/*
+ * This file is part of PharIo\Version.
+ *
+ * (c) Arne Blankerts <arne@blankerts.de>, Sebastian Heuer <sebastian@phpeople.de>, Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
-final class Version implements Extension\VersionExtension
-{
-    private Extension\NumberExtension $numberExtension;
-    /**
-     * @var string[]
-     */
-    private array $semverCommonPreReleaseIdentifiers = ['alpha', 'beta', 'rc'];
+namespace PharIo\Version;
 
-    public function __construct(?Extension\NumberExtension $numberExtension = null)
-    {
+class Version {
+    /** @var string */
+    private $originalVersionString;
 
-        $this->numberExtension = $numberExtension ?: new  Number();
+    /** @var VersionNumber */
+    private $major;
+
+    /** @var VersionNumber */
+    private $minor;
+
+    /** @var VersionNumber */
+    private $patch;
+
+    /** @var null|PreReleaseSuffix */
+    private $preReleaseSuffix;
+
+    /** @var null|BuildMetaData */
+    private $buildMetadata;
+
+    public function __construct(string $versionString) {
+        $this->ensureVersionStringIsValid($versionString);
+        $this->originalVersionString = $versionString;
     }
 
     /**
-     * Represents v2.0.0 of the semantic versioning: https://semver.org/spec/v2.0.0.html
+     * @throws NoPreReleaseSuffixException
      */
-    public function semver(bool $preRelease = false, bool $build = false): string
-    {
-        return sprintf(
-            '%d.%d.%d%s%s',
-            $this->numberExtension->numberBetween(0, 9),
-            $this->numberExtension->numberBetween(0, 99),
-            $this->numberExtension->numberBetween(0, 99),
-            $preRelease && $this->numberExtension->numberBetween(0, 1) === 1 ? '-' . $this->semverPreReleaseIdentifier() : '',
-            $build && $this->numberExtension->numberBetween(0, 1) === 1 ? '+' . $this->semverBuildIdentifier() : '',
+    public function getPreReleaseSuffix(): PreReleaseSuffix {
+        if ($this->preReleaseSuffix === null) {
+            throw new NoPreReleaseSuffixException('No pre-release suffix set');
+        }
+
+        return $this->preReleaseSuffix;
+    }
+
+    public function getOriginalString(): string {
+        return $this->originalVersionString;
+    }
+
+    public function getVersionString(): string {
+        $str = \sprintf(
+            '%d.%d.%d',
+            $this->getMajor()->getValue() ?? 0,
+            $this->getMinor()->getValue() ?? 0,
+            $this->getPatch()->getValue() ?? 0
         );
+
+        if (!$this->hasPreReleaseSuffix()) {
+            return $str;
+        }
+
+        return $str . '-' . $this->getPreReleaseSuffix()->asString();
+    }
+
+    public function hasPreReleaseSuffix(): bool {
+        return $this->preReleaseSuffix !== null;
+    }
+
+    public function equals(Version $other): bool {
+        if ($this->getVersionString() !== $other->getVersionString()) {
+            return false;
+        }
+
+        if ($this->hasBuildMetaData() !== $other->hasBuildMetaData()) {
+            return false;
+        }
+
+        if ($this->hasBuildMetaData() && $other->hasBuildMetaData() &&
+            !$this->getBuildMetaData()->equals($other->getBuildMetaData())) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function isGreaterThan(Version $version): bool {
+        if ($version->getMajor()->getValue() > $this->getMajor()->getValue()) {
+            return false;
+        }
+
+        if ($version->getMajor()->getValue() < $this->getMajor()->getValue()) {
+            return true;
+        }
+
+        if ($version->getMinor()->getValue() > $this->getMinor()->getValue()) {
+            return false;
+        }
+
+        if ($version->getMinor()->getValue() < $this->getMinor()->getValue()) {
+            return true;
+        }
+
+        if ($version->getPatch()->getValue() > $this->getPatch()->getValue()) {
+            return false;
+        }
+
+        if ($version->getPatch()->getValue() < $this->getPatch()->getValue()) {
+            return true;
+        }
+
+        if (!$version->hasPreReleaseSuffix() && !$this->hasPreReleaseSuffix()) {
+            return false;
+        }
+
+        if ($version->hasPreReleaseSuffix() && !$this->hasPreReleaseSuffix()) {
+            return true;
+        }
+
+        if (!$version->hasPreReleaseSuffix() && $this->hasPreReleaseSuffix()) {
+            return false;
+        }
+
+        return $this->getPreReleaseSuffix()->isGreaterThan($version->getPreReleaseSuffix());
+    }
+
+    public function getMajor(): VersionNumber {
+        return $this->major;
+    }
+
+    public function getMinor(): VersionNumber {
+        return $this->minor;
+    }
+
+    public function getPatch(): VersionNumber {
+        return $this->patch;
     }
 
     /**
-     * Common pre-release identifier
+     * @psalm-assert-if-true BuildMetaData $this->buildMetadata
+     * @psalm-assert-if-true BuildMetaData $this->getBuildMetaData()
      */
-    private function semverPreReleaseIdentifier(): string
-    {
-        $ident = Extension\Helper::randomElement($this->semverCommonPreReleaseIdentifiers);
-
-        if ($this->numberExtension->numberBetween(0, 1) !== 1) {
-            return $ident;
-        }
-
-        return $ident . '.' . $this->numberExtension->numberBetween(1, 99);
+    public function hasBuildMetaData(): bool {
+        return $this->buildMetadata !== null;
     }
 
     /**
-     * Common random build identifier
+     * @throws NoBuildMetaDataException
      */
-    private function semverBuildIdentifier(): string
-    {
-        if ($this->numberExtension->numberBetween(0, 1) === 1) {
-            // short git revision syntax: https://git-scm.com/book/en/v2/Git-Tools-Revision-Selection
-            return substr(sha1(Extension\Helper::lexify('??????')), 0, 7);
+    public function getBuildMetaData(): BuildMetaData {
+        if (!$this->hasBuildMetaData()) {
+            throw new NoBuildMetaDataException('No build metadata set');
         }
 
-        // date syntax
-        return DateTime::date('YmdHis');
+        return $this->buildMetadata;
+    }
+
+    /**
+     * @param string[] $matches
+     *
+     * @throws InvalidPreReleaseSuffixException
+     */
+    private function parseVersion(array $matches): void {
+        $this->major = new VersionNumber((int)$matches['Major']);
+        $this->minor = new VersionNumber((int)$matches['Minor']);
+        $this->patch = isset($matches['Patch']) ? new VersionNumber((int)$matches['Patch']) : new VersionNumber(0);
+
+        if (isset($matches['PreReleaseSuffix']) && $matches['PreReleaseSuffix'] !== '') {
+            $this->preReleaseSuffix = new PreReleaseSuffix($matches['PreReleaseSuffix']);
+        }
+
+        if (isset($matches['BuildMetadata'])) {
+            $this->buildMetadata = new BuildMetaData($matches['BuildMetadata']);
+        }
+    }
+
+    /**
+     * @param string $version
+     *
+     * @throws InvalidVersionException
+     */
+    private function ensureVersionStringIsValid($version): void {
+        $regex = '/^v?
+            (?P<Major>0|[1-9]\d*)
+            \\.
+            (?P<Minor>0|[1-9]\d*)
+            (\\.
+                (?P<Patch>0|[1-9]\d*)
+            )?
+            (?:
+                -
+                (?<PreReleaseSuffix>(?:(dev|beta|b|rc|alpha|a|patch|p|pl)\.?\d*))
+            )?
+            (?:
+                \\+
+                (?P<BuildMetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-@]+)*)
+            )?
+        $/xi';
+
+        if (\preg_match($regex, $version, $matches) !== 1) {
+            throw new InvalidVersionException(
+                \sprintf("Version string '%s' does not follow SemVer semantics", $version)
+            );
+        }
+
+        $this->parseVersion($matches);
     }
 }
