e2d0d4ef617dd2ec22a81cd9434ad5366caf034d
Turikrisztian <148880706+Turikrisztian@users.noreply.github.com>
2025-11-20 18:47:21 +0100

    Revert "1"
    This reverts commit 18eb3bf3f5380148955a32b66457de7b1a71812c.

--- messages.php
+++
@@ -1,6 +0,0 @@
-<?php
-return [
-    // Minutes to offset displayed message timestamps. Set MESSAGE_DISPLAY_OFFSET_MINUTES=60 in .env if you
-    // intentionally want them to appear one hour later than creation. Default 0 (immediate display).
-    'display_offset_minutes' => (int) env('MESSAGE_DISPLAY_OFFSET_MINUTES', 0),
-];
